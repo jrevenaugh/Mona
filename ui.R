504f7264968bf657571bd4a0ccba91db2ecce979
Justin Revenaugh <justinr@umn.edu>
2018-04-27 16:25:06 -0500

    Reformatted for easier reading.

--- ui.R
+++ ui.R
@@ -1,15 +1,21 @@
-ui <- shinyUI( fluidPage(
-  h4( "Mona Lisa SVD" ),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("sv", "Singular Values", min = 1,
-                  max = 50, value = c(1, 10), step = 1 ),
-      plotOutput( "SVPlot" )
-    ),
-
-    # Plot panel
-    mainPanel(
-      plotOutput( "FMona", height = "auto", width = "auto" )
+ui <- shinyUI(
+  fluidPage(
+    h4("Mona Lisa SVD"),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(inputId = "sv",
+                    label = "Singular Values",
+                    min = 1, max = 50, step = 1,
+                    value = c(1,10)
+        ),
+        plotOutput(outputId = "SVPlot")
+      ),
+      mainPanel(
+        plotOutput(outputId = "FMona",
+                   height = "auto",
+                   width = "auto"
+        )
+      )
     )
   )
-))
+)
